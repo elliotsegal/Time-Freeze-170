486e4c8b2ee126509c3dc0c73660a5595dfd7f84
elliotsegal <segal.elliot@hotmail.com>
2019-10-16 20:00:22 -0700

    Unity project setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed;
+    public float jumpSpeed;
+
+    private Rigidbody2D body;
+    private Vector2 input;
+    private bool jump;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        jump |= Input.GetButtonDown("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = body.velocity;
+        bool running = Mathf.Abs(input.x) > 0.5f;
+        if (running)
+        {
+            velocity.x = Mathf.Sign(input.x) * runSpeed;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
+        if (jump)
+        {
+            velocity.y = jumpSpeed;
+            jump = false;
+        }
+        body.velocity = velocity;
+    }
+}
